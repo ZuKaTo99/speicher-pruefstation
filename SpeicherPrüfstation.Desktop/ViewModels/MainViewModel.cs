1fcdd0aa260a68bead5b920a0d8e47af4778e172
Birkan Sakar <xboxliveboy@web.de>
2026-09-07 13:52:29 +0200

    feat(ui): add MVVM Status interaction

--- SpeicherPrüfstation.Desktop/ViewModels/MainViewModel.cs
+++ SpeicherPrüfstation.Desktop/ViewModels/MainViewModel.cs
@@ -1,9 +1,22 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace SpeicherPrüfstation.Desktop.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
     [ObservableProperty]
-    public partial string Greeting { get; set; } = "Linux Speicher-Prüfstation";
+    public partial string Greeting { get; set; } =
+        "Linux Speicher-Prüfstation";
+
+    [ObservableProperty]
+    public partial string StatusMessage { get; set; } =
+        "Bereit.";
+
+    [RelayCommand]
+    private void TestStatus()
+    {
+        StatusMessage =
+            "Die Verbindung zwischen Oberfläche und C# funktioniert.";
+    }
 }
